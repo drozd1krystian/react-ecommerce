a8ae578f96a6aeb530626fb24c5ab9243087bbc1
Krystian <drozd1krystian@gmail.com>
2020-08-08 19:58:16 +0200

    product details styling

--- src/Pages/ProductDetails/index.js
+++ src/Pages/ProductDetails/index.js
@@ -3,12 +3,42 @@ import "./style.scss";
 
 import { useParams } from "react-router-dom";
 
+import pro from "../../assets/nike20.json";
+
 const ProductDetails = (props) => {
-  let { productId } = useParams();
+  // let { productId } = useParams();
+
+  let product = pro[0];
 
   return (
-    <div>
-      <h1>Product details {productId}</h1>
+    <div className="content">
+      <div className="images">
+        {product.images.map((el) => (
+          <div class="img">
+            <img src={el} alt={product.productName} />
+          </div>
+        ))}
+      </div>
+      <div className="product-details">
+        <h2 className="py2">{product.productName}</h2>
+        <span>{product.salePrice} $</span>
+        <p className="py2">{product.description}</p>
+
+        <p>Pick your size:</p>
+        <div className="sizes mb2">
+          {product.sizes.map((el) => (
+            <div className="size">
+              <input type="radio" value={el} name="size" id={`size-${el}`} />
+              <label for={`size-${el}`}>
+                <span>EU {el}</span>
+              </label>
+            </div>
+          ))}
+        </div>
+        <button className="btn-light">
+          <span>Add to cart</span>
+        </button>
+      </div>
     </div>
   );
 };
